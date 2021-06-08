9b8ff57a48527da54d166f83d567b1e4514a4b89
Ritik Soni <47344024+ritiksoni00@users.noreply.github.com>
2021-06-08 17:29:04 +0530

    Update wsgi.py

--- result_pro_sys/wsgi.py
+++ result_pro_sys/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'result_pro_sys.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '.result_pro_sys.settings')
 
 application = get_wsgi_application()
