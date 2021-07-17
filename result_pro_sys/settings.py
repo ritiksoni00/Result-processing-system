cd4d72aec6261132dfdd8bc026f75b0f302d83c8
Ritik Soni <47344024+ritiksoni00@users.noreply.github.com>
2021-07-17 18:20:01 +0530

    Update settings.py

--- result_pro_sys/settings.py
+++ result_pro_sys/settings.py
@@ -15,7 +15,7 @@ import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ROOT_PATH = os.path.dirname(__file__)
 
 
@@ -68,7 +68,7 @@ TEM_DIR='/home/ritik/college-proj/rps/templates'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEM_DIR],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
