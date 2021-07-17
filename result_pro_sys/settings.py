18e78c841662ce99e76d4264fdd84c2c91f7e9b2
Ritik Soni <47344024+ritiksoni00@users.noreply.github.com>
2021-07-17 16:48:39 +0530

    Update settings.py

--- result_pro_sys/settings.py
+++ result_pro_sys/settings.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 import os
-import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_PATH = os.path.dirname(__file__)
 
-django_heroku.settings(locals())
 
 
 
